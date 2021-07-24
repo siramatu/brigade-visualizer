089b812322c1ac75332336755558cfa4fb526b2e
Kazuho Ohta <otkzh77@gmail.com>
2021-07-24 18:03:47 +0900

    nuxtのサンプル作成

---
+++ nuxt-sample/layouts/default.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer v-model="drawer" app> </v-navigation-drawer>
+
+    <!-- タイトルバー -->
+    <v-app-bar absolute color="amber" dark elevate-on-scroll>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>CivicTech俯瞰図鑑</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <!-- ナビゲーション -->
+    <v-navigation-drawer v-model="drawer" absolute temporary dark class="amber">
+      <v-list nav dense>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
---
+++ nuxt-sample/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <!-- メインページ -->
+    <v-main>
+      <v-flex>
+        <v-img src="~/assets/img/top.svg"></v-img>
+      </v-flex>
+    </v-main>
+  </v-app>
+</template>
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    items: [
+      { title: 'Home', icon: 'mdi-home', link: '#' },
+      { title: 'About', icon: 'mdi-help-box', link: '#' },
+    ],
+  }),
+}
+</script>
